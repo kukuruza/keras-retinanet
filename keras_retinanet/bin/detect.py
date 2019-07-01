bc18743f6db153d7040ebcc4bc8e09c15f0fb00f
Evgeny Toropov <toropov.evgeny@gmail.com>
2019-07-01 19:13:34 -0400

    detect.py works properly for shuffler dataset

--- keras_retinanet/bin/detect.py
+++ keras_retinanet/bin/detect.py
@@ -1,12 +1,13 @@
 import keras
 import sys, os
 from glob import glob
-from progressbar import progressbar
+import progressbar
 import imageio
+import argparse
 
-#import os, sys, os.path as op
-#sys.path.append(op.join(os.getenv('SHUFFLER_DIR'), 'interface'))
-#from kerasInterface import BareImageGenerator
+import os, sys, os.path as op
+sys.path.append(op.join(os.getenv('SHUFFLER_DIR'), 'interface'))
+from interfaceKeras import BareImageGenerator
 
 # Allow relative imports when being executed as script.
 if __name__ == "__main__" and __package__ is None:
@@ -27,12 +28,30 @@ import os
 import numpy as np
 import time
 
+progressbar.streams.wrap_stdout()
+
+parser = argparse.ArgumentParser(description='Detect boxes in images from Shuffler database')
+parser.add_argument('-i', '--in_db_file',
+        default='../stamps-1800x1200-empty-1class.db')
+parser.add_argument('--rootdir',
+        help='Where image files in the db are relative to.',
+        default='..')
+parser.add_argument('--out_dir',
+        help='The path of the output directory. ',
+        default='examples/detected/epoch10-test')
+parser.add_argument('--model_path',
+        help='The path of the trained model .h5 file.',
+        default='snapshots/from-coco-weights/resnet50_coco_10.h5')
+args = parser.parse_args()
+
+
 # Get data generator.
-#generator = BareImageGenerator(
-#        db_file='labelme/Images/HistoricalDocuments-1800x1200/258-00-01.jpg',
-#        rootdir='/home/etoropov/projects/HistoricalDocuments',
-#        batch_size=32,
-#        shuffle=False)
+generator = BareImageGenerator(
+        db_file=args.in_db_file,
+        rootdir=args.rootdir,
+        batch_size=32,
+        shuffle=False)
+
 
 # set tf backend to allow memory to grow, instead of claiming everything
 import tensorflow as tf
@@ -43,49 +62,62 @@ def get_session():
   return tf.Session(config=config)
 
 # use this environment flag to change which GPU to use
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+#os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 # set the modified tf session as backend in keras
 keras.backend.tensorflow_backend.set_session(get_session())
 
 print ('Loading model...')
-model_path = os.path.join('snapshots/from-coco-weights/resnet50_coco_10.h5')
-model = models.load_model(model_path, backbone_name='resnet50')
+model = models.load_model(args.model_path, backbone_name='resnet50')
 model = models.convert_model(model)
 
-labels_to_names = {0: 'stamp'}
+#labels_to_names = {0: 'stamp'}
 
-image_pattern = '../datasets/stamps-1800x1200-1class-empty/*.jpg'
-image_paths = glob(image_pattern)
+writer = imageio.get_writer(args.out_dir + '.mp4', fps=1, codec='mjpeg')
 
-writer = imageio.get_writer('examples/detected/epoch10-test.avi', fps=1)
+for batch in progressbar.progressbar(generator):
+  images = batch['image']
+  imagefiles = batch['imagefile']
+  draws = []
+  scales = []
 
-for image_path in progressbar(image_paths):
-
-    print ('Preprocessing image...')
-    image = read_image_bgr(image_path)
+  print ('Preprocessing batch... ', end='')
+  start = time.time()
+  for i, image in enumerate(images):
 
     draw = image.copy()
-    draw = cv2.cvtColor(draw, cv2.COLOR_BGR2RGB)
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
     # preprocess image for network
     image = preprocess_image(image)
     image, scale = resize_image(image)
 
-    # process image
-    print ('Predicting image...')
-    start = time.time()
-    output = model.predict_on_batch(np.expand_dims(image, axis=0))
-    boxes, scores, labels = output
-    print("processing time: ", time.time() - start)
+    images[i] = image
+    draws.append(draw)
+    scales.append(scale)
+
+
+  images = np.stack(images)
+  #print (images.shape)
+
+  print("\tin %.2f sec." % (time.time() - start))
+
+  # process images
+  print ('Predicting batch... ', end='')
+  start = time.time()
+  boxess, scoress, labelss = model.predict_on_batch(images)
+  print("\tin %.2f sec." % (time.time() - start))
+
+  print ('Postprocessing batch.. ', end='')
+  for image, imagefile, draw, scale, boxes, scores, labels in zip(images, imagefiles, draws, scales, boxess, scoress, labelss):
 
     # correct for image scale
     boxes /= scale
-    print (boxes)
-    print (scores.max())
+#    print (boxes)
+#    print (scores.max())
 
     # visualize detections
-    for box, score in zip(boxes[0], scores[0]):
+    for box, score in zip(boxes, scores):
         label = 0
         # scores are sorted so we can break
         if score < 0.5:
@@ -96,12 +128,15 @@ for image_path in progressbar(image_paths):
         b = box.astype(int)
         draw_box(draw, b, color=color)
         
-        caption = "{} {:.3f}".format(labels_to_names[label], score)
+        caption = "%.3f" % score
+        #caption = "{} {:.3f}".format(labels_to_names[label], score)
         draw_caption(draw, b, caption)
         
-    out_image_path = os.path.join('examples/detected/epoch10-test', os.path.basename(image_path))
+    out_image_path = os.path.join(args.out_dir, os.path.basename(imagefile))
     imageio.imwrite(out_image_path, draw)
     writer.append_data(draw)
+
+  print("\tin %.2f sec." % (time.time() - start))
 
 writer.close()
 
